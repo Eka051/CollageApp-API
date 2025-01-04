ff5b7a72b9407b4d6878c846778eecaa773f82f2
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2025-01-04 19:25:34 +0700

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using CollageApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollageApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Student> GetStudents()
+        {
+            return CollegeRepository.Students;
+        }
+
+        [HttpGet("{id:int}")]
+        public Student getStudentById(int id)
+        {
+            return CollegeRepository.Students.Where(n => n.Id == id).FirstOrDefault();
+        }
+    }
+}
---
+++ Models/CollegeRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace CollageApp.Models
+{
+    public static class CollegeRepository
+    {
+        public static List<Student> Students { get; set; } = new List<Student>
+        {
+            new Student { Id = 1, StudentName = "John", Email = "student1@gmail.com", Address = "USA" },
+            new Student { Id = 2, StudentName = "Smith", Email = "student2@gmail.com", Address = "UK" },
+            new Student { Id = 3, StudentName = "Doe", Email = "student3@gmail.com", Address = "Canada" }
+        };
+    }
+}
