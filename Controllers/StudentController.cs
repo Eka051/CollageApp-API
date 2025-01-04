c07dcb61f0a21513f1545ba5a9dbedbec1fc12d4
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2025-01-04 19:55:26 +0700

    feat: add some method and custom routing

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -8,15 +8,30 @@ namespace CollageApp.Controllers
     public class StudentController : ControllerBase
     {
         [HttpGet]
+        [Route("All", Name = "GetAllStudents")]
         public IEnumerable<Student> GetStudents()
         {
             return CollegeRepository.Students;
         }
 
-        [HttpGet("{id:int}")]
-        public Student getStudentById(int id)
+        [HttpGet]
+        [Route("{id}", Name = "GetStudentById")]
+        public Student GetStudentById(int id)
         {
             return CollegeRepository.Students.Where(n => n.Id == id).FirstOrDefault();
+        }
+
+        [HttpGet("{name}", Name = "GetStudentByName")]
+        public Student GetStudentByName(string name)
+        {
+            return CollegeRepository.Students.Where(n => n.StudentName.Contains(name)).FirstOrDefault();
+        }
+        [HttpDelete("{id}", Name = "DeleteStudentById")]
+        public bool DeleteStudent(int id)
+        {
+            var student = CollegeRepository.Students.Where(n => n.Id == id).FirstOrDefault();
+            CollegeRepository.Students.Remove(student);
+            return true;
         }
     }
 }
