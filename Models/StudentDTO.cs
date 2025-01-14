cbec2b1b2a14a5a429ed203a312f77d55fd2f990
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2025-01-14 11:02:47 +0700

    feat: add some model validation

--- Models/StudentDTO.cs
+++ Models/StudentDTO.cs
@@ -7,11 +7,27 @@ namespace CollegeApp.Models
     {
         [ValidateNever]
         public int Id { get; set; }
+
         [Required(ErrorMessage = "Student Name is Required. Fill this field!")]
+        [StringLength(50, MinimumLength = 2)]
         public string StudentName { get; set; }
+
         [EmailAddress(ErrorMessage = "Please enter valid email address (user@example.com)")]
         public string Email { get; set; }
+
+        [Range(16,30)]
+        public int age { get; set; }
+
         [Required(ErrorMessage = "Address is Required")]
+        [StringLength(100, MinimumLength = 5)]
         public string Address { get; set; }
+
+        [Required(ErrorMessage = "Password is Required")]
+        [StringLength(20, MinimumLength = 3)]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+
     }
 }
