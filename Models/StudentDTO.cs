1d692d0e3d714ad1061c7ec86e7de49086e15e93
Dian Eka Raharjo <dianekaraharjo@gmail.com>
2025-01-14 10:48:38 +0700

    refactor: update validation model with error message

--- Models/StudentDTO.cs
+++ Models/StudentDTO.cs
@@ -1,15 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace CollegeApp.Models
 {
     public class StudentDTO
     {
+        [ValidateNever]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Student Name is Required. Fill this field!")]
         public string StudentName { get; set; }
-        [EmailAddress]
+        [EmailAddress(ErrorMessage = "Please enter valid email address (user@example.com)")]
         public string Email { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Address is Required")]
         public string Address { get; set; }
     }
 }
